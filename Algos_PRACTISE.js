9830cd2110ef8d52039298b916b89a3e1ab4eee9
Elena <ponomarevaer@gmail.com>
2021-06-22 19:49:26 -0500

    Jun22

--- Algos_PRACTISE.js
+++ Algos_PRACTISE.js
@@ -218,5 +218,22 @@
 //     return arr;
 // }
 // arr = [1,3,5,7]
-
 // insertXinY(arr);
+
+// RemovingNegatives
+const removingNegatives = (arr) => {
+    var count = 0;
+    for (var i=0; i<arr.length; i++){
+        if (arr[i] < 0){
+            count ++;
+        } else {
+            arr[i - count] = arr[i];
+        }
+    }
+    while (count--){
+        arr.pop()
+    }
+    return arr;
+}
+arr = [0, -1, 2, -3, 4, -5, 6]
+console.log(removingNegatives(arr));
